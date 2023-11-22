6d42f319264dba19a302d9c78f509830ef152dc4
fauruz <le.phong1456@gmail.com>
2023-11-22 16:14:39 +0700

    bai1.2

--- bai1/Form1.cs
+++ bai1/Form1.cs
@@ -12,9 +12,141 @@ namespace bai1
 {
     public partial class Form1 : Form
     {
+        List<Question> lq = new List<Question>();
         public Form1()
         {
             InitializeComponent();
+        }
+        private void FormClear()
+        {
+            txtQuestion.Text = string.Empty;
+            rdbA.Text = string.Empty;
+            rdbB.Text = string.Empty;
+            rdbC.Text = string.Empty;
+            rdbD.Text = string.Empty;
+        }
+        private Question getQuestion()
+        {
+            if (txtQuestion.Text != string.Empty && txtA.Text != string.Empty && txtB.Text != string.Empty && txtC.Text != string.Empty && txtD.Text != string.Empty && cbbAnswer.SelectedIndex != -1)
+            {
+                Question qt = new Question();
+                qt.Content = txtQuestion.Text;
+                qt.A = txtA.Text;
+                qt.B = txtB.Text;
+                qt.C = txtC.Text;
+                qt.D = txtD.Text;
+                if (cbbAnswer.SelectedIndex.ToString() == "A")
+                    qt.Answer = txtA.Text;
+                else if(cbbAnswer.SelectedIndex.ToString() == "B")
+                    qt.Answer = txtB.Text;
+                else if(cbbAnswer.SelectedIndex.ToString().Equals("C"))
+                    qt.Answer = txtC.Text;
+                else
+                    qt.Answer = txtD.Text;
+                return qt;
+            }
+            else
+            {
+                MessageBox.Show("Nhập dữ liệu!");
+                return null;
+            }
+        }
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+
+            Question qt = getQuestion();
+            lq.Add(qt);
+            qt.Id = lq.Count();
+            textChange(qt);
+            comboBoxUpdate();
+        }
+        private void textChange(Question qt)
+        {
+            lblQuestion.Text = qt.Content;
+            rdbA.Visible = true;
+            rdbB.Visible = true;
+            rdbC.Visible = true;
+            rdbD.Visible = true;
+            btnSubmit.Visible = true;
+            rdbA.Text = qt.A;
+            rdbB.Text = qt.B;
+            rdbC.Text = qt.C;
+            rdbD.Text = qt.D;
+        }
+        private void comboBoxUpdate()
+        {
+            cbbQuestion.Items.Add("Câu hỏi " + lq.Count.ToString());
+            cbbQuestion.Show();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Question qt1 = new Question(1,"You like me?","A. Yes","B. Of course!","C. No","D. Absolutely no!","B");
+            Question qt2 = new Question(2, "Lương tháng con bao nhiêu?", "Dạ, 3 triệu", "Cô hỏi làm gì?", "Dạ, 20 triệu", "Đi mà hỏi con cô ấy", "A");
+            Question qt3 = new Question(3, "Nào con cưới?", "Mai", "Con độc thân", "Con đi tu", "Liên quan gì cô?", "B");
+            Question qt4 = new Question(4, "Con sửa giúp cô cái máy lạnh đi?", "How?", "Con học IT cô ơi", "Giải thích blah blah...", "Không", "D");
+            lq.Add(qt1);
+            comboBoxUpdate();
+            lq.Add(qt2);
+            comboBoxUpdate();
+            lq.Add(qt3);
+            comboBoxUpdate();
+            lq.Add(qt4);
+            comboBoxUpdate();
+            rdbA.Visible = false;
+            rdbB.Visible = false;
+            rdbC.Visible = false;
+            rdbD.Visible = false;
+            btnSubmit.Visible = false;
+            cbbAnswer.Items.Add("A");
+            cbbAnswer.Items.Add("B");
+            cbbAnswer.Items.Add("C");
+            cbbAnswer.Items.Add("D");
+        }
+
+        private void cbbQuestion_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            foreach(var item in lq)
+            {
+                if(cbbQuestion.SelectedIndex == item.Id-1)
+                {
+                    textChange(item);
+                }
+            }
+        }
+
+        private void rdbB_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            foreach (var item in lq)
+            {
+                if (cbbQuestion.SelectedIndex == item.Id - 1)
+                {
+                    if(rdbA.Checked && rdbA.Text == item.Answer)
+                    {
+                        MessageBox.Show("Đúng!");
+                    }
+                    else if (rdbB.Checked && rdbB.Text == item.Answer)
+                    {
+                        MessageBox.Show("Đúng!");
+                    }
+                    else if (rdbC.Checked && rdbC.Text == item.Answer)
+                    {
+                        MessageBox.Show("Đúng!");
+                    }
+                    else if (rdbD.Checked && rdbD.Text == item.Answer)
+                    {
+                        MessageBox.Show("Đúng!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Sai!");
+                    }    
+                }
+            }
         }
     }
 }
--- bai1/Question.cs
+++ bai1/Question.cs
@@ -8,10 +8,58 @@ namespace bai1
 {
     internal class Question
     {
+        public int Id {  get; set; }
         public string Content { get; set; }
         public string A { get; set; }
         public string B { get; set; }
         public string C { get; set; }
         public string D { get; set; }
+        public string Answer {  get; set; }
+        public Question(int id, string content, string a, string b, string c, string d, string answer)
+        {
+            Id = id;
+            Content = content;
+            A = a;
+            B = b;
+            C = c;
+            D = d;
+            if (answer == "A")
+            {
+                Answer = a;
+            }
+            else if (answer == "B")
+            {
+                Answer = b;
+            }
+            else if (answer == "C")
+            {
+                Answer = c;
+            }
+            else
+                Answer = d;
+        }
+        public Question() 
+        {
+            Id = 0;
+            Content = string.Empty;
+            A = string.Empty;
+            B = string.Empty;
+            C = string.Empty;
+            D = string.Empty;
+            if (Answer == "A")
+            {
+                Answer = A;
+            }
+            else if (Answer == "B")
+            {
+                Answer = B;
+            }
+            else if (Answer == "C")
+            {
+                Answer = C;
+            }
+            else
+                Answer = D;
+        }
     }
 }
