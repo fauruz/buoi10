5ef88cd1237445c2b1bc6a679642bb6119820c87
fauruz <le.phong1456@gmail.com>
2023-11-22 13:05:19 +0700

    bai1.1

---
+++ bai1/Question.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bai1
+{
+    internal class Question
+    {
+        public string Content { get; set; }
+        public string A { get; set; }
+        public string B { get; set; }
+        public string C { get; set; }
+        public string D { get; set; }
+    }
+}
